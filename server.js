90b3379cb79c6b563a7cc9061e1dbbc0f46c1345
Wisnu Andika <wisnuandika104@gmail.com>
2023-10-25 10:00:39 +0700

    add unit testing with jest

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require("body-parser");
-const routerTodo = require('./routes/todo.route')
-const sequelize = require('./config/db_connect')
+const routerTodo = require('./src/routes/todo.route')
+const sequelize = require('./src/config/db_connect')
 require('dotenv').config()
 
 const app = new express()
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use('/todo', routerTodo)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Running on http://localhost:${process.env.PORT}`)
-})
+if(process.env.NODE_ENV != "test") {
+  app.listen(process.env.PORT, () => {
+    console.log(`Running on http://localhost:${process.env.PORT}`)
+  })
+}
+ 
+module.exports = app
